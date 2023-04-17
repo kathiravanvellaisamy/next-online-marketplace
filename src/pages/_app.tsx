f28011d3e3760beb78107f3a18da782583d0db56
kathiarvanvellaisamy <kathiarvanvellaisamy@gmail.com>
2023-04-17 22:20:52 +0530

    add navbar also add new model for storing listing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { NavBar } from "~/components/Header";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <NavBar />
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
